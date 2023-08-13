33d9b7808faefe791db831470527529b8f0f56ea
vetan2 <vetan2@naver.com>
2023-08-13 18:57:04 +0900

    feat: Add react rules

--- packages/eslint-config-react/index.ts
+++ packages/eslint-config-react/index.ts
@@ -7,4 +7,23 @@ export = {
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
   ],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    "react/jsx-curly-brace-presence": [
+      1,
+      {
+        props: "never",
+        children: "never",
+      },
+    ],
+    "react/prop-types": 0,
+    "react/require-default-props": 0,
+  },
 } satisfies ESLint.ConfigData;
