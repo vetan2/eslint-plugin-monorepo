4a9cf8ff45a8219a989f0df1fc9fcd8d4e9a8e2b
vetan2 <vetan2@naver.com>
2023-08-13 18:09:42 +0900

    fix: Fix overrides conflict

--- packages/eslint-config-react/index.ts
+++ packages/eslint-config-react/index.ts
@@ -1,16 +1,10 @@
 import type { ESLint } from "eslint";
 
 export = {
-  extends: ["@vetan2/ts"],
-  overrides: [
-    {
-      files: ["*.ts", "*.tsx"],
-      plugins: ["react", "react-hooks"],
-      extends: [
-        "@vetan2/ts",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-      ],
-    },
+  plugins: ["react", "react-hooks"],
+  extends: [
+    "@vetan2/ts",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
   ],
 } satisfies ESLint.ConfigData;
