cc68f0973c667417e32fee18ebe759955298ad3e
vetan2 <vetan2@naver.com>
2023-08-13 17:30:19 +0900

    feat: Add react config

--- packages/eslint-plugin/index.ts
+++ packages/eslint-plugin/index.ts
@@ -1,10 +1,12 @@
 import typescriptConfig from "@vetan2/eslint-config-ts";
 import baseConfig from "@vetan2/eslint-config-base";
+import reactConfig from "@vetan2/eslint-config-react";
 import type { ESLint } from "eslint";
 
 export = {
   configs: {
     base: baseConfig,
     ts: typescriptConfig,
+    react: reactConfig,
   },
 } satisfies ESLint.Plugin;
